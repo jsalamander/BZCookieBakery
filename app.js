abda114947d4d4c92151a943951124c7837c7dff
jf <jan.friedli@immerda.ch>
2022-03-03 17:59:31 +0100

    improve logging fix some docker issues

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const Sentry = require('@sentry/node');
 const cron = require('node-cron');
-const log = require('simple-node-logger').createSimpleLogger();
 const helpers = require('./helpers');
-
-log.setLevel(process.env.NODE_ENV === 'production' ? 'info' : 'debug');
+const log = require('./logger');
 
 // every 8th hour
 const defaultCron = process.env.CRON_COOKIE_FETCH_EXPR || '0 */8 * * *';
@@ -21,7 +19,7 @@ if (process.env.SENTRY_DSN || false) {
 let cookieStore = {};
 
 cron.schedule(defaultCron, async () => {
-  cookieStore = await helpers.bakeCookies(cookieStore, log);
+  cookieStore = await helpers.bakeCookies(cookieStore);
 });
 
 const app = express();
@@ -31,7 +29,7 @@ const port = process.env.PORT || 3000;
 app.get('/', async (req, res) => {
   res.contentType('application/json');
   const todayStamp = Date.now();
-  cookieStore = Object.keys(cookieStore)
+  cookieStore = Object.keys(cookieStore || {})
     .filter((key) => key > todayStamp)
     .reduce((obj, key) => {
       /* eslint-disable-next-line no-param-reassign */
@@ -47,6 +45,6 @@ app.get('/', async (req, res) => {
 app.listen(port, async () => {
   log.info(`BZCookieBakery taking orders at http://localhost:${port}`);
   log.info('starting initial bakery cookie fetch jobs');
-  cookieStore = await helpers.bakeCookies(cookieStore, log);
+  cookieStore = await helpers.bakeCookies(cookieStore);
   log.debug('initial cookie store', cookieStore);
 });
